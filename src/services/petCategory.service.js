195f5dc931d07437b3ae0bce9b725bde4cbe319e
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-11-28 11:22:54 +0700

    Check null in pet category (#86)

--- src/services/petCategory.service.js
+++ src/services/petCategory.service.js
@@ -10,11 +10,13 @@ function createPetCategory(data) {
   const { image, ...otherData } = data;
   return prisma.createPetCategory({
     ...otherData,
-    image: {
-      connect: {
-        id: image,
-      },
-    },
+    image: image
+      ? {
+          connect: {
+            id: image,
+          },
+        }
+      : null,
   });
 }
 
